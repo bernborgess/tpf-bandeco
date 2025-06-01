ebbba37a4c323ee89c7c46a4941129970be3efa5
bernborgess <bernborgess@outlook.com>
2025-06-01 19:46:01 -0300

    World building with assets

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,8 +1,8 @@
 #include "Game.h"
 
 // Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 448;
+const int SCREEN_WIDTH = 960; 
+const int SCREEN_HEIGHT = 720;
 
 int main(int argc, char** argv) {
     Game game = Game(SCREEN_WIDTH, SCREEN_HEIGHT);
