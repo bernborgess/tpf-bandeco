8149f1cf93dba4d1ff3787bc51a9d6ffa3a18adb
bernborgess <bernborgesse@outlook.com>
2025-07-06 23:58:06 -0300

    Add Cabinet States

--- Source/Blocks/Cabinet.h
+++ Source/Blocks/Cabinet.h
@@ -7,7 +7,14 @@ class Cabinet : public Block {
     Cabinet(Game* game, std::pair<int, int> gridPos)
         : Block(game, "../Assets/Prototype/Cabinet.png", gridPos) {}
 
+    void OnUpdate(float deltaTime) override;
+
     int mDirtyPlateCount = 0;
 
     Item* PickItemOnTop() override;
+
+   private:
+    static const std::string CABINET_EMPTY_PATH;
+    static const std::string CABINET_PLATE_PATH;
+    static const std::string CABINET_PLATES_PATH;
 };
